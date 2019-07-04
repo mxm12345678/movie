cc08a2f9d124477ffbf66cc797308fc315fd6ddf
hehu <hehu>
2019-07-04 11:06:49 +0800

    hehu

---
+++ pages/subPages/about-page/about-page.js
@@ -0,0 +1,26 @@
+Page({
+  data:{
+    info:[
+      {
+        label:'姓名',
+        value:'何虎'
+      },
+      {
+        label: '职业',
+        value: 'web前端'
+      },
+      {
+        label: 'QQ',
+        value: '1530883347'
+      },
+      {
+        label: '微信',
+        value: 'hehu1996'
+      },
+      {
+        label: 'github',
+        value: 'hehu1996'
+      }
+    ]
+  }
+})
